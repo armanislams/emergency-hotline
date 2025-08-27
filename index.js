62fe748f132a56a86545c438f8082ccbb8ccf60b
ArmanIslamTC <armanislam.ais@gmail.com>
2025-08-28 00:50:23 +0800

    call btn  function added

--- index.js
+++ index.js
@@ -19,3 +19,32 @@ function deductCoins(amount) {
     balanceElement.innerText = currentBalance;
     return currentBalance;
 }
+// call btn
+const callHistory = [];
+const charge = 20;
+const callBtns = document.getElementsByClassName("call-btn");
+
+    for (const btn of callBtns) {
+        btn.addEventListener("click", function () {
+            const availableBalance = parseInt(document.getElementById("coin-count").innerText);
+            // const currentBalance = availableBalance;
+            if (availableBalance < charge) {
+            alert("Insufficient balance to make a call.");
+            return;
+        }
+        document.getElementById("coin-count").innerText = availableBalance - charge;
+        alert(
+            "Calling... " +
+            this.parentElement.parentElement.querySelector("h2").innerText +
+            " " +
+            this.parentElement.parentElement.querySelector("h3").innerText
+        );
+        const data = {
+        name: this.parentElement.parentElement.querySelector("h2").innerText,
+        number: this.parentElement.parentElement.querySelector("h3").innerText,
+        date: new Date().toLocaleTimeString(),
+    }
+    callHistory.push(data);
+    history();
+    });
+}
